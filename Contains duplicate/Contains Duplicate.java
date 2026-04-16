c62f6f01bc1572f3b110ff0bbd2928a5bc7c72c7
rishaba <rishabamanickamoorthi24@gmail.com>
2026-04-16 11:07:03 +0530

    contains duplicate using hashmap

---
+++ Contains duplicate/Contains Duplicate.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap <Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+
+
+            if(map.containsKey(nums[i])) // already exists
+            {
+                return true;
+            }
+
+            map.put(nums[i], 1);
+        }
+
+
+        return false; // no duplicate
+
+    }
+}
