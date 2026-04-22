edc600025d11fb31b4f4c50dae5a7555089d2993
rishaba <rishabamanickamoorthi24@gmail.com>
2026-04-22 10:36:28 +0530

    upper bound

---
+++ Upper bound/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    int upperBound(int[] arr, int target) {
+        // code here
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1;   // left side
+            } else {
+                low = mid + 1;    // right side
+            }
+        }
+
+        return ans;
+    }
+}
