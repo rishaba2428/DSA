4da1636117c37992782b414c71afd182e7fa2e9e
rishaba <rishabamanickamoorthi24@gmail.com>
2026-05-06 15:18:18 +0530

    happy number

---
+++ Happy Number/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n){
+        while(n!=1 && n!=4)
+        {
+            int sum =0;
+            while(n>0)
+            {
+                int count = n % 10;
+                sum =sum + count * count;
+                n=n/10;
+            }
+
+            n=sum;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
