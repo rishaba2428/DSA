aeefd1a9618918ec10b79580ff10eba89d6ac1b0
rishaba2428 <rishabamanickamoorthi24@gmail.com>
2026-07-09 23:52:52 +0530

    Find Smallest Letter Greater Than Target

---
+++ Find Smallest Letter Greater Than Target/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        int low = 0;
+        int high = letters.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (letters[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if (low == letters.length) {
+            return letters[0];
+        }
+
+        return letters[low];
+    }
+}
